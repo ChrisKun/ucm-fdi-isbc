f720acc3c44e627d41f9cdca11cacafee75a84c6
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2012-11-21 19:04:38 +0000

    Funciona ya la parte de la query.
    Me falta profundizar más en el tema de la función de similitud global y necesito tener un fichero con los partidos de la jornada a jugar, asi como otro con la clasificación de los equipos.

--- trunk/src/proyecto2/quinielas/Quinielas.java
+++ trunk/src/proyecto2/quinielas/Quinielas.java
@@ -1,12 +1,31 @@
 package proyecto2.quinielas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import javax.swing.JOptionPane;
+
+import proyecto2.quinielas.representacion.DescripcionQuinielas;
 import jcolibri.casebase.LinealCaseBase;
 import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.Attribute;
 import jcolibri.cbrcore.CBRCase;
 import jcolibri.cbrcore.CBRCaseBase;
 import jcolibri.cbrcore.CBRQuery;
 import jcolibri.cbrcore.Connector;
 import jcolibri.exception.ExecutionException;
+import jcolibri.method.gui.formFilling.ObtainQueryWithFormMethod;
+import jcolibri.method.retrieve.RetrievalResult;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+import jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import jcolibri.method.retrieve.NNretrieval.similarity.global.Average;
+import jcolibri.method.retrieve.NNretrieval.similarity.local.Equal;
+import jcolibri.method.retrieve.NNretrieval.similarity.local.Interval;
+import jcolibri.method.retrieve.selection.SelectCases;
 
 public class Quinielas implements StandardCBRApplication {
 
@@ -15,51 +34,141 @@ public class Quinielas implements StandardCBRApplication {
 	
 	@Override
 	public void configure() throws ExecutionException {
+		try{
+		//Crear el conector con la base de casos
 		connector = new ConnectorQuinielas();
+		//La organización en memoria será lineal
 		caseBase = new LinealCaseBase();
-
+		} catch (Exception e){
+			throw new ExecutionException(e);
+		}	
 	}
 
 	@Override
-	public CBRCaseBase preCycle() throws ExecutionException {
-		caseBase.init(connector);
-		
-		// AquÃ­ se calcularÃ­a datos sobre los casos como los puntos acumulados, etc
-		// caseBase.getCases();
-		
+	public CBRCaseBase preCycle() throws ExecutionException {		
+		// Cargar los casos desde el conector a la base de casos
+		caseBase.init(connector);		
 		return caseBase;
 	}
 
 	@Override
 	public void cycle(CBRQuery query) throws ExecutionException {
-		// TODO Auto-generated method stub
-
+		// Para configurar el KNN se utiliza un objeto NNConfig
+		NNConfig simConfig = new NNConfig();
+		
+		// Fijamos la función de similitud global
+		simConfig.setDescriptionSimFunction(new Average());		
+		
+		// Fijamos las funciones de similitud locales
+		simConfig.addMapping(new Attribute("temporada", DescripcionQuinielas.class), new Interval(13));
+		simConfig.addMapping(new Attribute("local", DescripcionQuinielas.class), new Equal());
+		simConfig.addMapping(new Attribute("visitante", DescripcionQuinielas.class), new Equal());
+		simConfig.addMapping(new Attribute("puntosLocal", DescripcionQuinielas.class), new Interval(126));
+		simConfig.addMapping(new Attribute("pgLocal", DescripcionQuinielas.class), new Interval(42));
+		simConfig.addMapping(new Attribute("peLocal", DescripcionQuinielas.class), new Interval(42));
+		simConfig.addMapping(new Attribute("ppLocal", DescripcionQuinielas.class), new Interval(42));
+		simConfig.addMapping(new Attribute("puntosVisitante", DescripcionQuinielas.class), new Interval(126));
+		simConfig.addMapping(new Attribute("pgVisitante", DescripcionQuinielas.class), new Interval(42));
+		simConfig.addMapping(new Attribute("peVisitante", DescripcionQuinielas.class), new Interval(42));
+		simConfig.addMapping(new Attribute("ppVisitante", DescripcionQuinielas.class), new Interval(42));
+		simConfig.addMapping(new Attribute("posLocal", DescripcionQuinielas.class), new Interval(22));
+		simConfig.addMapping(new Attribute("posVisitante", DescripcionQuinielas.class),	new Interval(22));
+		simConfig.addMapping(new Attribute("gfavorLocal", DescripcionQuinielas.class), new Interval(130));
+		simConfig.addMapping(new Attribute("gcontraLocal", DescripcionQuinielas.class), new Interval(120));
+		simConfig.addMapping(new Attribute("gfavorVisitante", DescripcionQuinielas.class), new Interval(130));
+		simConfig.addMapping(new Attribute("gcontraVisitante", DescripcionQuinielas.class), new Interval(120));
+		
+		// Asignamos pesos
+		simConfig.setWeight(new Attribute("temporada", DescripcionQuinielas.class), 0.3);
+		simConfig.setWeight(new Attribute("local", DescripcionQuinielas.class), 0.02);
+		simConfig.setWeight(new Attribute("visitante", DescripcionQuinielas.class), 0.02);
+		simConfig.setWeight(new Attribute("puntosLocal", DescripcionQuinielas.class), 0.2);
+		simConfig.setWeight(new Attribute("pgLocal", DescripcionQuinielas.class), 0.1);
+		simConfig.setWeight(new Attribute("peLocal", DescripcionQuinielas.class), 0.025);
+		simConfig.setWeight(new Attribute("ppLocal", DescripcionQuinielas.class), 0.05);
+		simConfig.setWeight(new Attribute("puntosVisitante", DescripcionQuinielas.class), 0.25);
+		simConfig.setWeight(new Attribute("pgVisitante", DescripcionQuinielas.class), 0.15);
+		simConfig.setWeight(new Attribute("peVisitante", DescripcionQuinielas.class), 0.025);
+		simConfig.setWeight(new Attribute("ppVisitante", DescripcionQuinielas.class), 0.01);
+		simConfig.setWeight(new Attribute("posLocal", DescripcionQuinielas.class), 0.3);
+		simConfig.setWeight(new Attribute("posVisitante", DescripcionQuinielas.class), 0.3);
+		simConfig.setWeight(new Attribute("gfavorLocal", DescripcionQuinielas.class), 0.2);
+		simConfig.setWeight(new Attribute("gcontraLocal", DescripcionQuinielas.class), 0.4);
+		simConfig.setWeight(new Attribute("gfavorVisitante", DescripcionQuinielas.class), 0.4);
+		simConfig.setWeight(new Attribute("gcontraVisitante", DescripcionQuinielas.class), 0.2);
+		
+		// Ejecutamos la recuperación del vecino más próximo
+		Collection<RetrievalResult> eval = NNScoringMethod.evaluateSimilarity(caseBase.getCases(), query, simConfig);
+		
+		// Seleccionamos los k mejores casos
+		eval = SelectCases.selectTopKRR(eval, 5);
+		
+		Collection<CBRCase> casos = new ArrayList<CBRCase>();
+		System.out.println("Casos Recuperados:");
+		for (RetrievalResult nse: eval){
+			System.out.println(nse);
+			casos.add(nse.get_case());
+		}
 	}
 
 	@Override
 	public void postCycle() throws ExecutionException {
-		// TODO Auto-generated method stub
-
+		this.caseBase.close();
 	}
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		Quinielas q = new Quinielas();
-		CBRCaseBase casebase;
 		try {
+			//Configuración
 			q.configure();
-			casebase = q.preCycle();
-			for(CBRCase c : casebase.getCases())
-				System.out.println(c);
-		
+			// Preciclo
+			q.preCycle();
+			//Crear un objeto que almacena la consulta
+			CBRQuery query = new CBRQuery();
+			// Rellenamos el HashMap con la información de los equipos
+			HashMap<String, int[]> hash = rellenaHash(query);
+			do {
+				// Ejemplo de equipos
+				String equipoLocal = "Athletic";
+				String equipoVisitante = "Getafe";
+				rellenaQuery(query,hash,equipoLocal,equipoVisitante);		
+				//Ejecutar el ciclo
+				q.cycle(query);
+			} while (JOptionPane.showConfirmDialog(null, "Continuar?")==JOptionPane.OK_OPTION);
 		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
-		}
-		
-
+		}	
 	}
 
+	private static HashMap<String, int[]> rellenaHash (CBRQuery query){		
+		try {
+			// Rellenamos la información de los equipos de la jornada pedida
+			BufferedReader br = new BufferedReader(new FileReader("jornada_actual.txt"));			
+			String line = null;
+			HashMap<String, int[]> hash = new HashMap<String, int[]>();
+			while((line=br.readLine())!=null){					
+				String[] tokens = line.split(",");
+				int[] valores=new int[8];
+				int i=0;
+				while (i < 7) {
+					valores[i] = Integer.valueOf(tokens[i+1]); 					
+					i++;
+				}
+				hash.put(tokens[0],valores);				
+			}
+			br.close();
+			return hash;
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+		return null;		
+	}
+	
+	private static void rellenaQuery(CBRQuery query, HashMap<String, int[]> hash, String local, String visitante) {
+		int [] clasifLocal = hash.get(local);
+		int [] clasifVisitante = hash.get(visitante);
+		DescripcionQuinielas queryDesc = new DescripcionQuinielas (2012,local,clasifLocal,visitante,clasifVisitante);		
+		query.setDescription(queryDesc);		
+	}
 }
--- trunk/src/proyecto2/quinielas/representacion/DescripcionQuinielas.java
+++ trunk/src/proyecto2/quinielas/representacion/DescripcionQuinielas.java
@@ -3,40 +3,67 @@ package proyecto2.quinielas.representacion;
 import jcolibri.cbrcore.Attribute;
 import jcolibri.cbrcore.CaseComponent;
 
-public class DescripcionQuinielas implements CaseComponent {
+public class DescripcionQuinielas implements CaseComponent {	
 
+	// Numero de campos que tenemos
+	public final static int NUMCAMPOS = 20;
 	private int id;
-	private String temporada;
-	private String liga;
-	private int jornada;
+	private int temporada;
 	private String local;
 	private String visitante;
-	private int golesLocal;
-	private int golesVisitante;
+	private int puntosLocal;
+	private int pgLocal;
+	private int peLocal;
+	private int ppLocal;
+	private int puntosVisitante;
+	private int pgVisitante;
+	private int peVisitante;
+	private int ppVisitante;
+	private int posLocal;
+	private int posVisitante;
+	private int gfavorLocal;
+	private int gcontraLocal;
+	private int gfavorVisitante;
+	private int gcontraVisitante;
+
+	public DescripcionQuinielas(int temporada,String local, int[] clasifLocal, String visitante, int[] clasifVisitante) {
+		this.temporada = temporada;
+		this.local = local;
+		this.visitante = visitante;
+		this.posLocal = clasifLocal[1];		
+		this.pgLocal = clasifLocal[2];
+		this.peLocal = clasifLocal[3];
+		this.ppLocal = clasifLocal[4];
+		this.gfavorLocal = clasifLocal[5];
+		this.gcontraLocal = clasifLocal[6];
+		this.puntosLocal = clasifLocal[7];
+		this.posVisitante = clasifVisitante[1];		
+		this.pgVisitante = clasifVisitante[2];
+		this.peVisitante = clasifVisitante[3];
+		this.ppVisitante = clasifVisitante[4];
+		this.gfavorVisitante = clasifVisitante[5];
+		this.gcontraVisitante = clasifVisitante[6];
+		this.puntosVisitante = clasifVisitante[7];
+	}
 	
-	
-	
-	
-	public int getGolesLocal() {
-		return golesLocal;
+	@Override
+	public String toString() {
+		return "DescripcionQuinielas [id=" + id + ", temporada=" + temporada
+				+ ", local=" + local + ", visitante=" + visitante
+				+ ", puntosLocal=" + puntosLocal + ", pgLocal=" + pgLocal
+				+ ", peLocal=" + peLocal + ", ppLocal=" + ppLocal
+				+ ", puntosVisitante=" + puntosVisitante + ", pgVisitante="
+				+ pgVisitante + ", peVisitante=" + peVisitante
+				+ ", ppVisitante=" + ppVisitante + ", posLocal=" + posLocal
+				+ ", posVisitante=" + posVisitante + ", gfavorLocal="
+				+ gfavorLocal + ", gcontraLocal=" + gcontraLocal
+				+ ", gfavorVisitante=" + gfavorVisitante
+				+ ", gcontraVisitante=" + gcontraVisitante + "]";
 	}
 
 
-
-	public void setGolesLocal(int golesLocal) {
-		this.golesLocal = golesLocal;
-	}
-
-
-
-	public int getGolesVisitante() {
-		return golesVisitante;
-	}
-
-
-
-	public void setGolesVisitante(int golesVisitante) {
-		this.golesVisitante = golesVisitante;
+	public DescripcionQuinielas() {
+		super();
 	}
 
 
@@ -53,38 +80,14 @@ public class DescripcionQuinielas implements CaseComponent {
 
 
 
-	public String getTemporada() {
+	public int getTemporada() {
 		return temporada;
 	}
 
 
 
-	public void setTemporada(String temporada) {
+	public void setTemporada(int temporada) {
 		this.temporada = temporada;
-	}
-
-
-
-	public String getLiga() {
-		return liga;
-	}
-
-
-
-	public void setLiga(String liga) {
-		this.liga = liga;
-	}
-
-
-
-	public int getJornada() {
-		return jornada;
-	}
-
-
-
-	public void setJornada(int jornada) {
-		this.jornada = jornada;
 	}
 
 
@@ -110,6 +113,176 @@ public class DescripcionQuinielas implements CaseComponent {
 	public void setVisitante(String visitante) {
 		this.visitante = visitante;
 	}
+
+
+
+	public int getPuntosLocal() {
+		return puntosLocal;
+	}
+
+
+
+	public void setPuntosLocal(int puntosLocal) {
+		this.puntosLocal = puntosLocal;
+	}
+
+
+
+	public int getPgLocal() {
+		return pgLocal;
+	}
+
+
+
+	public void setPgLocal(int pgLocal) {
+		this.pgLocal = pgLocal;
+	}
+
+
+
+	public int getPeLocal() {
+		return peLocal;
+	}
+
+
+
+	public void setPeLocal(int peLocal) {
+		this.peLocal = peLocal;
+	}
+
+
+
+	public int getPpLocal() {
+		return ppLocal;
+	}
+
+
+
+	public void setPpLocal(int ppLocal) {
+		this.ppLocal = ppLocal;
+	}
+
+
+
+	public int getPuntosVisitante() {
+		return puntosVisitante;
+	}
+
+
+
+	public void setPuntosVisitante(int puntosVisitante) {
+		this.puntosVisitante = puntosVisitante;
+	}
+
+
+
+	public int getPgVisitante() {
+		return pgVisitante;
+	}
+
+
+
+	public void setPgVisitante(int pgVisitante) {
+		this.pgVisitante = pgVisitante;
+	}
+
+
+
+	public int getPeVisitante() {
+		return peVisitante;
+	}
+
+
+
+	public void setPeVisitante(int peVisitante) {
+		this.peVisitante = peVisitante;
+	}
+
+
+
+	public int getPpVisitante() {
+		return ppVisitante;
+	}
+
+
+
+	public void setPpVisitante(int ppVisitante) {
+		this.ppVisitante = ppVisitante;
+	}
+
+
+
+	public int getPosLocal() {
+		return posLocal;
+	}
+
+
+
+	public void setPosLocal(int posLocal) {
+		this.posLocal = posLocal;
+	}
+
+
+
+	public int getPosVisitante() {
+		return posVisitante;
+	}
+
+
+
+	public void setPosVisitante(int posVisitante) {
+		this.posVisitante = posVisitante;
+	}
+
+
+
+	public int getGfavorLocal() {
+		return gfavorLocal;
+	}
+
+
+
+	public void setGfavorLocal(int gfavorLocal) {
+		this.gfavorLocal = gfavorLocal;
+	}
+
+
+
+	public int getGcontraLocal() {
+		return gcontraLocal;
+	}
+
+
+
+	public void setGcontraLocal(int gcontraLocal) {
+		this.gcontraLocal = gcontraLocal;
+	}
+
+
+
+	public int getGfavorVisitante() {
+		return gfavorVisitante;
+	}
+
+
+
+	public void setGfavorVisitante(int gfavorVisitante) {
+		this.gfavorVisitante = gfavorVisitante;
+	}
+
+
+
+	public int getGcontraVisitante() {
+		return gcontraVisitante;
+	}
+
+
+
+	public void setGcontraVisitante(int gcontraVisitante) {
+		this.gcontraVisitante = gcontraVisitante;
+	}
+
+
 
 	public Attribute getIdAttribute() {
 		return new Attribute("id",DescripcionQuinielas.class);
