f9af9d04f4f6f14704751f8c9ee74ac9baf4bf4a
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2012-11-22 19:58:52 +0000

    - Retocados algunos fallitos

--- src/proyecto2/quinielas/ValidacionCruzada.java
+++ src/proyecto2/quinielas/ValidacionCruzada.java
@@ -12,8 +12,9 @@ public class ValidacionCruzada {
 	    jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), HoldOutEvaluator.class);
 	       
 	    HoldOutEvaluator eval = new HoldOutEvaluator();
-	    eval.init(new Quinielas());
-	    eval.HoldOut(1, 1);
+	    eval.init(new Quinielas(true));
+	    // Configurar % de casos que cogemos y nº de vueltas
+	    eval.HoldOut(5, 1);
 	    
 	    
 	    System.out.println(Evaluator.getEvaluationReport());
@@ -27,7 +28,7 @@ public class ValidacionCruzada {
 		jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), LeaveOneOutEvaluator.class);
 		
 		LeaveOneOutEvaluator eval = new LeaveOneOutEvaluator();
-		eval.init(new Quinielas());
+		eval.init(new Quinielas(true));
 		eval.LeaveOneOut();
 		    
 		System.out.println(Evaluator.getEvaluationReport());
@@ -41,7 +42,7 @@ public class ValidacionCruzada {
 		jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), SameSplitEvaluator.class);
 		            
 		SameSplitEvaluator eval = new SameSplitEvaluator();
-		eval.init(new Quinielas());
+		eval.init(new Quinielas(true));
 		eval.generateSplit(10, "split1.txt");
 		eval.HoldOutfromFile("split1.txt");
 		            
