50d55b25367853316f9af18c9082d07683c98a16
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2012-11-23 12:17:01 +0000

    - Añadida clase Config.java para el intercambio de información

---
+++ src/proyecto2/quinielas/Config.java
@@ -0,0 +1,56 @@
+package proyecto2.quinielas;
+
+/**
+ * 
+ * Clase que contiene la última información disponible en la Web y la información que solicita el usuario.
+ *
+ */
+
+public class Config {
+	
+	private Integer ultimaTemporada;
+	private Integer ultimaJornadaPrimera;
+	private Integer ultimaJornadaSegunda;
+	
+	private Integer seleccionTemporada;
+	private Integer seleccionJornadaPrimera;
+	private Integer seleccionJornadaSegunda;
+	
+	public Integer getUltimaTemporada() {
+		return ultimaTemporada;
+	}
+	public void setUltimaTemporada(Integer ultimaTemporada) {
+		this.ultimaTemporada = ultimaTemporada;
+	}
+	public Integer getUltimaJornadaPrimera() {
+		return ultimaJornadaPrimera;
+	}
+	public void setUltimaJornadaPrimera(Integer ultimaJornadaPrimera) {
+		this.ultimaJornadaPrimera = ultimaJornadaPrimera;
+	}
+	public Integer getUltimaJornadaSegunda() {
+		return ultimaJornadaSegunda;
+	}
+	public void setUltimaJornadaSegunda(Integer ultimaJornadaSegunda) {
+		this.ultimaJornadaSegunda = ultimaJornadaSegunda;
+	}
+	public Integer getSeleccionTemporada() {
+		return seleccionTemporada;
+	}
+	public void setSeleccionTemporada(Integer seleccionTemporada) {
+		this.seleccionTemporada = seleccionTemporada;
+	}
+	public Integer getSeleccionJornadaPrimera() {
+		return seleccionJornadaPrimera;
+	}
+	public void setSeleccionJornadaPrimera(Integer seleccionJornadaPrimera) {
+		this.seleccionJornadaPrimera = seleccionJornadaPrimera;
+	}
+	public Integer getSeleccionJornadaSegunda() {
+		return seleccionJornadaSegunda;
+	}
+	public void setSeleccionJornadaSegunda(Integer seleccionJornadaSegunda) {
+		this.seleccionJornadaSegunda = seleccionJornadaSegunda;
+	}
+
+}
