ef3c5128c6bbfd2fb47d88f2d50973eff5e7e838
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2012-11-23 14:08:10 +0000

    - Añadidas librerias - Añadida clase Principal

--- src/proyecto2/quinielas/cbr/ValidacionCruzada.java
+++ src/proyecto2/quinielas/cbr/ValidacionCruzada.java
@@ -5,44 +5,44 @@ import jcolibri.evaluation.evaluators.*;
 
 public class ValidacionCruzada {
 	
-    public void HoldOutEvaluation()
+    public void HoldOutEvaluation(double[] listaPesos)
     {
 	    //SwingProgressBar shows the progress
 	    jcolibri.util.ProgressController.clear();
 	    jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), HoldOutEvaluator.class);
 	       
 	    HoldOutEvaluator eval = new HoldOutEvaluator();
-	    eval.init(new Quinielas(true));
+	    eval.init(new Quinielas(true, listaPesos));
 	    // Configurar % de casos que cogemos y nº de vueltas
-	    eval.HoldOut(1, 1);
+	    eval.HoldOut(5, 1);
 	    
 	    
 	    System.out.println(Evaluator.getEvaluationReport());
 	    jcolibri.evaluation.tools.EvaluationResultGUI.show(Evaluator.getEvaluationReport(), "Quinielas - Evaluation", false);
     }
     
-    public void LeaveOneOutEvaluation()
+    public void LeaveOneOutEvaluation(double[] listaPesos)
     {
 		//SwingProgressBar shows the progress
 		jcolibri.util.ProgressController.clear();
 		jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), LeaveOneOutEvaluator.class);
 		
 		LeaveOneOutEvaluator eval = new LeaveOneOutEvaluator();
-		eval.init(new Quinielas(true));
+		eval.init(new Quinielas(true, listaPesos));
 		eval.LeaveOneOut();
 		    
 		System.out.println(Evaluator.getEvaluationReport());
 		jcolibri.evaluation.tools.EvaluationResultGUI.show(Evaluator.getEvaluationReport(), "Quinielas - Evaluation", false);
     }
     
-    public void SameSplitEvaluation()
+    public void SameSplitEvaluation(double[] listaPesos)
     {
 		//SwingProgressBar shows the progress
 		jcolibri.util.ProgressController.clear();
 		jcolibri.util.ProgressController.register(new jcolibri.test.main.SwingProgressBar(), SameSplitEvaluator.class);
 		            
 		SameSplitEvaluator eval = new SameSplitEvaluator();
-		eval.init(new Quinielas(true));
+		eval.init(new Quinielas(true, listaPesos));
 		// Configurar el % de testeo y el nombre del fichero de salida
 		eval.generateSplit(10, "split1.txt");
 		eval.HoldOutfromFile("split1.txt");
