dc2367f12e0556cbfa8114a3867ead994fb5d956
xechu.rbs <xechu.rbs@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-01-11 14:03:20 +0000

    - Raúl: Organizada la Interfaz en clases diferentes.

---
+++ trunk/src/proyecto3/Interfaz/PanelInicio.java
@@ -0,0 +1,14 @@
+package Interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PanelInicio extends JPanel{
+	
+	public PanelInicio()
+	{
+	}
+}
