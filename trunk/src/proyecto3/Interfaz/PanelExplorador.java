1fd3bc7b1842ae1ecd984027fc9f78cfc5836cfe
xechu.rbs <xechu.rbs@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-01-17 18:02:57 +0000

    Raúl - Siguiendo trabajando con el PanelExplorador

--- trunk/src/proyecto3/Interfaz/PanelExplorador.java
+++ trunk/src/proyecto3/Interfaz/PanelExplorador.java
@@ -1,21 +1,27 @@
 package Interfaz;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class PanelExplorador extends JPanel implements ChangeListener {
 
-	private static final int MAX = 500;
+	private static final int MAX = 25000;
 	private static final int MIN = 0;
-	private double precioMinimo;
-	private double precioMaximo;
+	private int precioMinimo;
+	private int precioMaximo;
 	private JSlider barraPrecioMin;
 	private JSlider barraPrecioMax;
+	private JTextField etiquetaPrecioMax;
+	private JTextField etiquetaPrecioMin;
+	
 	//2 slider -> el precio marcado por el slider del precio maximo acota el precio máximo que alcanza el precio min
 	/*
 	 *   precio max 0|-----|50-----|200
@@ -26,8 +32,8 @@ public class PanelExplorador extends JPanel implements ChangeListener {
 	 */
 	
 	public PanelExplorador(){
-		precioMinimo = 1.04;
-		precioMaximo = 200.35;
+		precioMinimo = 10000;
+		precioMaximo = 20000;
 		this.setLayout(new BorderLayout());
 		this.add(getPanelFiltrado(), BorderLayout.NORTH);
 		this.add(getPanelArticulos(), BorderLayout.CENTER);
@@ -46,18 +52,15 @@ public class PanelExplorador extends JPanel implements ChangeListener {
 	}
 			
 	private JPanel getSubPanelFiltradoPrecio(){
-		JPanel panel = new JPanel(new GridLayout(2,1));
-		barraPrecioMax = new JSlider(JSlider.HORIZONTAL, MIN, MAX, (int)precioMaximo);
-		barraPrecioMin = new JSlider(JSlider.HORIZONTAL,MIN, (int)precioMaximo, (int) precioMinimo);
+		JPanel panel = new JPanel(new GridLayout(2,2));
+		barraPrecioMax = new JSlider(JSlider.HORIZONTAL, MIN, MAX, precioMaximo);
+		barraPrecioMin = new JSlider(JSlider.HORIZONTAL,MIN, precioMaximo, precioMinimo);
 		barraPrecioMax.addChangeListener(this);
-		barraPrecioMax.setMajorTickSpacing(50);
-		barraPrecioMax.setMinorTickSpacing(10);
 		barraPrecioMax.setPaintTicks(true);
 		barraPrecioMax.setPaintLabels(true);
-		barraPrecioMin.setMajorTickSpacing(50);
-		barraPrecioMin.setMinorTickSpacing(10);
 		barraPrecioMin.setPaintTicks(true);
 		barraPrecioMin.setPaintLabels(true);
+		panel.add(getSubPanelFiltradoPrecioMax());
 		panel.add(barraPrecioMax);
 		panel.add(barraPrecioMin);
 		//JSlider precio = new JSlider(JSlider.HORIZONTAL,FPS_MIN, FPS_MAX, FPS_INIT);
@@ -70,10 +73,27 @@ public class PanelExplorador extends JPanel implements ChangeListener {
 		return panel;
 	}
 	
+	private JPanel getSubPanelFiltradoPrecioMax()
+	{
+		JPanel p = new JPanel();
+		p.setLayout(new FlowLayout());
+		JLabel l = new JLabel("Precio máximo: ");
+		etiquetaPrecioMax = new JTextField(""+precioMaximo);
+		etiquetaPrecioMax.setEditable(false);
+		JLabel l2 = new JLabel("€");
+		
+		p.add(l);
+		p.add(etiquetaPrecioMax);
+		p.add(l2);
+		return p;
+	}
+	
 	@Override
 	public void stateChanged(ChangeEvent e) {
 		 JSlider source = (JSlider)e.getSource();
-		  barraPrecioMin.setMaximum(barraPrecioMax.getValue());
+		 precioMaximo = barraPrecioMax.getValue();
+		  barraPrecioMin.setMaximum(precioMaximo);
+		  etiquetaPrecioMax.setText(""+((double) precioMaximo/100));
 		
 	}
 	
