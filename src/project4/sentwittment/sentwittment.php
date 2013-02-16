7d2961f0a4af1c5ce6dca94e6960335f420c87e2
xavier.gallofre <xavier.gallofre@gmail.com>
2013-02-16 00:26:25 +0000

    First commit of the 4th project.

---
+++ src/project4/sentwittment/sentwittment.php
@@ -0,0 +1,79 @@
+<html>
+<body>
+
+<form action = "Prueba.php" method="post">
+Query: <input type="text" name="query"><br>
+ReturnPerPage: <input type="int" name="rpp"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+<?php //Código Principal
+
+$lexicon = loadLexicon();
+
+if (isset($_POST["query"]))
+{
+  $q = "q=" . $_POST["query"] ; // Concepto o Query
+  $rpp = "rpp=" . $_POST["rpp"]; // Return Per Page
+  $include_entities = "include_entities=false"; // Metadata about Tweets
+  $result_type = "result_type=recent"; // Popular and Recent
+
+  /* 
+  La búsqueda se realiza por consulta web con un 'search.json?' al que hay que
+  indicarle obligatoriamente un concepto con 'q= ' que representa el concepto
+  de la query
+  La llamada original al buscador sería con las siguientes opciones
+    'q= ' -> Concepto de la Query
+    'rpp= ' -> Numero de tweets por pagina
+    'include_entities= ' -> Para incluir metadata
+    'result_type= ' -> Tweets populares, recientes o mixtos
+  
+  La llamada Original
+    $format = "json?q=blue%20angels&rpp=5&include_entities=true&result_type=mixed";
+  */
+  $format = "json?".$q."&".$rpp."&".$include_entities."&".$result_type;
+  $filename = "http://search.twitter.com/search." . $format;
+
+  $json = file_get_contents($filename, true);
+  $decode = json_decode($json, true);
+
+  echo "<pre>";
+  $count = count($decode["results"]);
+  for($i=0;$i<$count;$i++){
+  //print_r($decode["results"][$i]["created_at"]."<br>");
+  echo $i . ":\t" . $decode["results"][$i]["text"]."<br>" ;
+  }
+  echo "</pre>";
+  }
+?>
+
+<?php // Functions
+
+function loadLexicon(){
+  $file = fopen("lexicon.txt", "r") or exit("Unable to open file!");
+  $lexicon = array();
+  while(!feof($file)){
+    addStringToLexicon($lexicon, fgets($file));
+  }
+  fclose($file);
+  return $lexicon;
+}
+
+function addStringToLexicon(&$lexicon,$string){
+  $parts = explode("\t",$string);
+  $lexicon[$parts[0]] = array("id" => $parts[1], "neg" => 0, "pos" => 0);
+  $count = count($parts);
+  if (strcmp($parts[2], "neg") == 0) $lexicon[$parts[0]]["neg"] = $lexicon[$parts[0]]["neg"]+1;
+  if (strcmp($parts[2], "pos") == 0) $lexicon[$parts[0]]["pos"] = $lexicon[$parts[0]]["pos"]+1;
+  if ($count == 4){
+    if (strcmp($parts[3], "pos") > 0){ 
+      $lexicon[$parts[0]]["pos"] = $lexicon[$parts[0]]["pos"]+1;
+    } else {
+      $lexicon[$parts[0]]["neg"] = $lexicon[$parts[0]]["neg"]+1;
+    }
+  }
+}
+?>
