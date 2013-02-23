234ecb27c51f511a6aecf24bb7758a0465e22412
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-02-23 18:00:20 +0000

    El programa ya reconoce las palabras y calcula los valores.
    Empieza viendo si la palabra está tal cual y si no aplica stemming

--- src/project4/sentwittment/sentwittment.php
+++ src/project4/sentwittment/sentwittment.php
@@ -18,8 +18,9 @@
 </html>
 
 <?php //Main Program
-
+require_once '/stemmer/Spanish.php';
 $lexicon = loadLexicon("lexicon.txt");
+
 if (!empty($_POST["query"]))
 {
 	$query = getQueryFromForm();
@@ -43,34 +44,35 @@ if (!empty($_POST["query"]))
     Loads the lexicon given by $file
 */
 function loadLexicon($file){
-  $file = fopen($file, "r") or exit("Unable to open file!");
-  $lexicon = array();
-  while(!feof($file)){
-    addStringToLexicon($lexicon, fgets($file));
-  }
-  fclose($file);
-  return $lexicon;
+	$file = fopen($file, "r") or exit("Unable to open file!");
+	$lexicon = array();
+	$pos = 0;
+	while(!feof($file)){
+		addStringToLexicon($lexicon, fgets($file));
+		$pos++;
+	}
+	fclose($file);
+	return $lexicon;
 }
 
 /**
     Adds info of $string in $lexicon
+	All the strings in the lexicon have the same two beginning parts: word & number in wordnet
+	We don't use the wordnet number
+	The resultant array is like: array['lexicon'] = ['stem' => X, 'neg' => Y, 'pos' => Z]
 */
 function addStringToLexicon(&$lexicon,$string){
-  $string = trim($string);
-  $parts = explode("\t",$string);
-  
-  //echo $parts[0] . " -> " . $parts[1] . "<br>"; 
-  $lexicon[$parts[0]] = array("id" => $parts[1], "neg" => 0, "pos" => 0);
-  $count = count($parts);
-  if (strcmp($parts[2], "neg") == 0) $lexicon[$parts[0]]["neg"] = $lexicon[$parts[0]]["neg"]+1;
-  if (strcmp($parts[2], "pos") == 0) $lexicon[$parts[0]]["pos"] = $lexicon[$parts[0]]["pos"]+1;
-  if ($count == 4){
-    if (strcmp($parts[3], "pos") == 0){ 
-      $lexicon[$parts[0]]["pos"] = $lexicon[$parts[0]]["pos"]+1;
-    } else {
-      $lexicon[$parts[0]]["neg"] = $lexicon[$parts[0]]["neg"]+1;
-    }
-  }
+	$string = trim($string);
+	$parts = explode("\t",$string);
+	$stem = spanish::stemm($parts[0]);
+	$lexicon[$parts[0]] = array("stem" => $stem, "neg" => 0, "pos" => 0);
+	for ($i=2;$i<count($parts);$i++) {
+		if (strcmp($parts[$i], "neg") == 0)
+			$lexicon[$parts[0]]["neg"] = $lexicon[$parts[0]]["neg"]+1;
+		else if (strcmp($parts[$i], "pos") == 0)
+			$lexicon[$parts[0]]["pos"] = $lexicon[$parts[0]]["pos"]+1;
+	}
+	//echo $parts[0] . " [" . $stem . " , " . $lexicon[$parts[0]]["neg"] . " , " . $lexicon[$parts[0]]["pos"] . "] " . "<br>"; 
 }
 
 /**
@@ -112,12 +114,11 @@ function cleanString($string){
     $patterns = array();
     $patterns[0] = '#http://[a-zA-Z0-9.\/]*#';
     $patterns[1] = '#\d#';
-    $patterns[2] = '#_-(),;.:@#';
-    //$patterns[2] = '#^[a-zA-Z]+#';
+    $patterns[2] = '#_|-|\+|\(|\)|\'|\"|,|;|\.|:|@|=|<|>|!|\?|\|ª|´"#';
 
     $string = preg_replace($patterns, "", $string);
-    //Removing unnecessary blanks
-    $string = preg_replace("#\n\t(\ )+#", " ", $string);
+	//Removing unnecessary blanks
+    $string = preg_replace("#(\n|\t|\ )+#", " ", $string);
     //$string = preg_replace("#\s#", " ", $string);
     $string = trim($string);
 
@@ -128,41 +129,61 @@ function cleanString($string){
     Consults a $string in $lexicon and returns a value
 */
 function consultLexicon($lexicon, $string){
-  $value = 0;
-  //Check if string is in Lexicon
-  //Recover values from Lexicon
-  //Return array of values
-  
-  return $value;
+	$value = 0;
+	//Check if string is in Lexicon
+	if (array_key_exists($string,$lexicon)) {
+		$value += $lexicon[$string]["pos"];
+		$value -= $lexicon[$string]["neg"];
+		echo 'si' . " " . $string . "<br>";
+	// If it isn't, then apply stemming
+	} else if (strlen($string) > 2){
+		$stem = spanish::stemm($string);
+		$value = checkStem($stem, $lexicon);		
+	} 
+	return $value;
 }
 
 /**
     Consults a $tweet in $lexicon and returns total value of $tweet
+	NOTA: seria la leche poder eliminar los acentos en la cadena global, pero no se porque me los cambia a cosas del estilo: A?a
 */
-function tweetValue($lexicon, $tweet){
-  $textCleaned = cleanString($tweet);
-  $words = explode(" ",$textCleaned); print_r($words);
-  applyStemming($words);
-  applyLemming($words);
-  $value = 0;
-  $count = count($words);
+function tweetValue($lexicon, $tweet){		
+	$textCleaned = cleanString($tweet);
+	$words = explode(" ",$textCleaned); print_r($words);
+	$value = 0;
+	$count = count($words);
 	for($i=0;$i<$count;$i++){
-        $value += consultLexicon($lexicon, $words[$i]);
+		$words[$i] = stripAccents($words[$i]);
+		$value += consultLexicon($lexicon, $words[$i]);
 	}
-  return $value;
+	echo $value . "<br>";	
+	return $value;
+}
+
+/************************ AUXILIARY FUNCTIONS ************************/ 
+
+/**
+	Replaces the accents and the Ò (in the lexicon the words doesn't have Ò, instead there are words with n)
+	Expample: daÒino -> danino
+*/
+function stripAccents($string){
+	$tofind = "¿¡¬√ƒ≈‡·‚„‰Â“”‘’÷ÿÚÛÙıˆ¯»… ÀËÈÍÎ«ÁÃÕŒœÏÌÓÔŸ⁄€‹˘˙˚¸ˇ—Ò";
+	$replac = "AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn";
+	return(strtr($string,$tofind,$replac));
 }
 
 /**
-    Apply Stemming to given array $words
+	Checks if $stem exists in $lexicon and operates the values of all the words in the lexicon with that stem
 */
-function applyStemming(&$words){
-
-}
-
-/**
-    Aplly Lemming to given array $words
-*/
-function applyLemming(&$words){
-
+function checkStem($stem, $lexicon){
+	$value = 0;
+	foreach ($lexicon as &$content) {
+		if (strcmp($stem, $content["stem"]) == 0) {
+			echo $stem . " raiz " . "si" . "<br>";
+			$value += $content["pos"];
+			$value -= $content["neg"];
+		}
+	}
+	return $value;
 }
 ?>
