fe1d1d8bbf82d6eebbf0c9ab6fdc00f964405f5b
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-02-26 19:57:14 +0000

    Changed a pair of things of the numbers in the formulary

--- src/project4/sentwittment/sentwittment.php
+++ src/project4/sentwittment/sentwittment.php
@@ -58,8 +58,6 @@ function tweetValue($lexicon, $tweet){
 		$words[$i]=cleanString($words[$i]);
 		$value += consultLexicon($lexicon, $words[$i]);
 	}
-	if($value > 0) $value = 1;
-	else if ($value < 0) $value = -1;
 	echo $value . "<br>";	
 	return $value;
 }
