5ba6ba12fe373726f72004f1fea4fe7ad0979652
xechu.rbs <xechu.rbs@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-05-05 17:15:00 +0000

    Empezado a adaptar todo para que las fotos sean individuos de la ontologia y la creacion de la tabla

---
+++ trunk/proyecto5/src/interfaz/TablaIndividuos.java
@@ -0,0 +1,20 @@
+package interfaz;
+
+import javax.swing.table.DefaultTableModel;
+
+public class TablaIndividuos extends DefaultTableModel {
+	
+	/**
+	 * Con el nombre de una foto, busca en la ontologia y si existe, rellena la tabla
+	 * con las propiedades y sus valores
+	 * @param foto
+	 */
+	public void actualizarTabla(String foto){
+		//1. Comprobar que existe la foto en la ontologia (como individuo)
+		//2. Ver las propiedades de la foto y cargar esos valores para cada propiedad (cogiendolo
+			// de ontobridge
+		//3. Añadir esta información a la tabla (actualizar)
+		
+	}
+
+}
