13ea1e0ef8998ae413b35fb72e7f79ba6eedb303
xechu.rbs <xechu.rbs@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-05-05 22:47:38 +0000

    Main actualizado (solo para pruebas)

--- proyecto5/src/clasificador/Main.java
+++ proyecto5/src/clasificador/Main.java
@@ -1,15 +1,25 @@
 package clasificador;
 
+import java.awt.BorderLayout;
+import java.util.Iterator;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 import ontobridge.Ontologia;
 import interfaz.Controlador;
+import interfaz.TablaIndividuos;
 import interfaz.VentanaPrincipal;
 
 public class Main {
 
 	public static final String rootPath = "proyecto5";
-	public static final String gamesPath = rootPath + "\\fotos";
-	private static final String pathOntologia = "file:proyecto5/src/ontologia/etiquetado.owl";
+	public static final String gamesPath = rootPath + "\\fotos"; //"fotos"; 
+	private static final String pathOntologia =  "file:proyecto5/src/ontologia/etiquetado.owl"; //"file:src/ontologia/etiquetado.owl";
 	private static final String urlOntologia = "http://http://sentwittment.p.ht/";
+	
 	
 	public static VentanaPrincipal vista;
 	
@@ -21,5 +31,34 @@ public class Main {
 		controlador.getTiposDeContenido();
 		
 		vista.setVisible(true);
+		
+		/* SOLO PARA PRUEBAS */
+		JFrame f = new JFrame();
+		JPanel p = new JPanel();
+		p.setLayout(new BorderLayout());
+		f.setContentPane(p);
+		TablaIndividuos tab = new TablaIndividuos(modelo);
+		JTable t = new JTable(tab);
+		/*
+		 * Métodos interesantes a la hora de añadir el default table model a un JTable
+		 */
+		t.setCellSelectionEnabled(false);
+		JScrollPane scrollPane = new JScrollPane(t);
+		t.setFillsViewportHeight(true);
+		p.add(BorderLayout.CENTER, scrollPane);
+		f.setVisible(true);
+		boolean para = false;
+		Iterator<String> it = modelo.getOb().listAllClasses();
+		while (it.hasNext() && !para){
+			String clase = it.next();
+			Iterator<String> l = modelo.getOb().listInstances(clase);
+			while (l.hasNext() && !para)
+			{
+				String la = l.next();
+				if (la != null)
+					para = true;
+				tab.actualizarTabla(la);
+			}
+		}
 	}
 }
