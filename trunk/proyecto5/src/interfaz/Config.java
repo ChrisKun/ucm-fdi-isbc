dd9e13fed3edabc15830798a3f54603f2ce16ea1
silentrowi <silentrowi@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-05-07 22:58:08 +0000

    Ya funca de puta madre

--- trunk/proyecto5/src/interfaz/Config.java
+++ trunk/proyecto5/src/interfaz/Config.java
@@ -10,5 +10,6 @@ public class Config {
 	public static final String aparece = "aparece";
 	public static final String apareceEn = "aparece_en";
 	public static final String urlfoto = "urlfoto";
-
+	public static final String saleEnFoto = "sale_en_foto";
+	public static final String juego = "Juego";
 }
