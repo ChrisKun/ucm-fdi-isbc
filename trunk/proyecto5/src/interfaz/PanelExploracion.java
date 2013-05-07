daf00c35645df2c71c5df704a2c50bd146c693e4
xavier.gallofre@gmail.com <xavier.gallofre@gmail.com@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-05-07 20:07:05 +0000

    Recolocacion del ontoTree y añadidos botones y confirmacion de cierre

--- trunk/proyecto5/src/interfaz/PanelExploracion.java
+++ trunk/proyecto5/src/interfaz/PanelExploracion.java
@@ -1,5 +1,6 @@
 package interfaz;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 
 import javax.swing.JPanel;
@@ -12,21 +13,33 @@ public class PanelExploracion extends JPanel{
 	public static final String panelIntro = "Introduccion";
 	public static final String panelExplorador = "Explorador de Imagenes";
 	public static final String panelFoto = "Informacion de la foto actual";
+	
+	private JPanel panelIntercambiable;
+	private PanelOntoTree panelOntoTree;
+	
 	private Explorador explorador;
 	private FotoActual fotoActual;
 	
 	public PanelExploracion(Controlador controlador){
 		explorador = new Explorador(Main.gamesPath);
 		fotoActual = new FotoActual(controlador);
-		this.setLayout(new CardLayout(10,10));
-		this.add(explorador, panelExplorador);
-		this.add(fotoActual, panelFoto);		
+		
+		panelIntercambiable = new JPanel();
+		panelIntercambiable.setLayout(new CardLayout(10,10));
+		panelIntercambiable.add(explorador, panelExplorador);
+		panelIntercambiable.add(fotoActual, panelFoto);
+		
+		this.setLayout(new BorderLayout());
+		this.add(panelIntercambiable,BorderLayout.CENTER);
+		
+		panelOntoTree = new PanelOntoTree(controlador);
+		this.add(panelOntoTree, BorderLayout.WEST);
 	}
 	
 	public void cambiarPanel(String nuevoPanel){
-		CardLayout cl = (CardLayout) this.getLayout();		
-		cl.show(this, nuevoPanel);
-		this.validate();
+		CardLayout cl = (CardLayout) panelIntercambiable.getLayout();		
+		cl.show(panelIntercambiable, nuevoPanel);
+		panelIntercambiable.validate();
 	}
 
 	public void setPathExplorador(String pathFile){
---
+++ trunk/proyecto5/src/interfaz/PanelOntoTree.java
@@ -0,0 +1,47 @@
+package interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class PanelOntoTree extends JPanel implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+
+	private JComboBox cB_Seleccion;
+	private String[] valoresSeleccion = {"Contenidos","Fotos"};
+	private Component ontoTree;
+	
+	
+	private Controlador controlador;
+	
+	public PanelOntoTree(Controlador controlador){
+		super();
+		this.controlador = controlador;
+		
+		this.setLayout(new BorderLayout(10,10));
+		
+		ontoTree = controlador.getTree();
+		this.add(ontoTree,BorderLayout.CENTER);
+		
+		cB_Seleccion = new JComboBox(valoresSeleccion);
+		cB_Seleccion.addActionListener(this);
+		this.add(cB_Seleccion, BorderLayout.NORTH);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JComboBox cB = (JComboBox) e.getSource();
+		if (cB.getSelectedItem() == valoresSeleccion[0]){
+			JOptionPane.showMessageDialog(this, "Cambia la raiz a Contenidos");
+		}
+		if (cB.getSelectedItem() == valoresSeleccion[1]){
+			JOptionPane.showMessageDialog(this, "Cambia la raiz a Fotos");
+		}
+	}
+}
