5798aa459d93a12b59bc57d620c855fde032d1cc
xavier.gallofre@gmail.com <xavier.gallofre@gmail.com@3b45b883-87c0-be31-e53a-b1a692650cbd>
2013-05-10 12:39:46 +0000

    Creado el proyecto para ISBCTournament

---
+++ trunk/proyecto6/src/behaviours/GoToBall.java
@@ -0,0 +1,38 @@
+package behaviours;
+
+import teams.ucmTeam.Behaviour;
+import teams.ucmTeam.RobotAPI;
+
+public class GoToBall extends Behaviour{
+
+	@Override
+	public void configure() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInit(RobotAPI arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRelease(RobotAPI arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int takeStep() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
